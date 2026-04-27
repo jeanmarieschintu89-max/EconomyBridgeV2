6ef6ad83727053dcfd8fea8059335ea6af9c18d4
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-04-27 22:29:00 +0200

    Update ShopListener.java

--- src/main/java/fr/moodcraft/bridge/ShopListener.java
+++ src/main/java/fr/moodcraft/bridge/ShopListener.java
@@ -3,23 +3,22 @@ package fr.moodcraft.bridge;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import com.ghostchu.quickshop.api.event.economy.ShopSuccessPurchaseEvent;
-import com.ghostchu.quickshop.api.shop.Shop;
+import com.ghostchu.quickshop.api.event.economy.EconomyTransactionEvent;
 
 public class ShopListener implements Listener {
 
     @EventHandler
-    public void onPurchase(ShopSuccessPurchaseEvent event) {
+    public void onTransaction(EconomyTransactionEvent event) {
 
         try {
-            Shop shop = event.getShop();
+            // Vérifie que c’est un achat (pas une vente)
+            if (!event.isPurchase()) return;
+
+            String item = event.getItem().getType().name().toLowerCase();
             int amount = event.getAmount();
 
-            String item = shop.getItem().getType().name().toLowerCase();
+            System.out.println("[Bridge] Transaction détectée -> " + item + " x" + amount);
 
-            System.out.println("[Bridge] Achat -> " + item + " x" + amount);
-
-            // Envoie vers Skript
             PriceUpdater.sendToSkript(item, amount);
 
         } catch (Exception e) {

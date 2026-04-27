fc6e6836dbb196d680a7cd369fdc4644db13fb30
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-04-27 17:32:53 +0200

    Update ShopListener.java

--- src/main/java/fr/moodcraft/bridge/ShopListener.java
+++ src/main/java/fr/moodcraft/bridge/ShopListener.java
@@ -5,16 +5,23 @@ import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import com.ghostchu.quickshop.api.event.ShopPurchaseEvent;
+import com.ghostchu.quickshop.api.event.ShopTransactionEvent;
 
 public class ShopListener implements Listener {
 
     @EventHandler
-    public void onBuy(ShopPurchaseEvent e) {
+    public void onTransaction(ShopTransactionEvent e) {
+
+        // 🔎 Vérifie que c’est un achat (et pas une vente)
+        if (!e.getAction().name().equalsIgnoreCase("BUY")) return;
 
         Material mat = e.getShop().getItem().getType();
         int amount = e.getAmount();
 
-        Bukkit.getLogger().info("Achat: " + mat + " x" + amount);
+        Bukkit.getLogger().info("Achat détecté: " + mat + " x" + amount);
+
+        // 👉 ici tu peux connecter ton économie
+        // exemple:
+        // add("diamond", amount);
     }
 }

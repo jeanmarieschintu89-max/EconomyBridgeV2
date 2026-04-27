34d9592b0bfb71e08e72e39dc71838df98a724c9
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-04-28 00:39:16 +0200

    Update PriceUpdater.java

--- src/main/java/fr/moodcraft/bridge/PriceUpdater.java
+++ src/main/java/fr/moodcraft/bridge/PriceUpdater.java
@@ -2,42 +2,29 @@ package fr.moodcraft.bridge;
 
 import com.ghostchu.quickshop.api.QuickShopAPI;
 import com.ghostchu.quickshop.api.shop.Shop;
+import org.bukkit.Bukkit;
 
 import java.util.Collection;
 
 public class PriceUpdater {
 
-    public static void updateItem(String item, double price) {
+    public static void updateItem(String item) {
+
+        Object value = ch.njol.skript.variables.Variables.getVariable("price." + item, null, false);
+
+        if (!(value instanceof Number)) return;
+
+        double price = ((Number) value).doubleValue();
 
         Collection<Shop> shops = QuickShopAPI.getInstance().getShopManager().getAllShops();
 
-        int updated = 0;
-
         for (Shop shop : shops) {
 
-            String shopItem = shop.getItem().getType().name().toLowerCase();
-
-            if (shopItem.equalsIgnoreCase(item)
-                    || normalize(shopItem).equalsIgnoreCase(item)) {
-
+            if (shop.getItem().getType().name().equalsIgnoreCase(item)) {
                 shop.setPrice(price);
-                updated++;
             }
         }
 
-        Main.getInstance().getLogger().info("[Bridge] Shops update " + item + " -> " + price + " (" + updated + " shops)");
-    }
-
-    private static String normalize(String id) {
-        switch (id) {
-            case "iron_ingot": return "iron";
-            case "gold_ingot": return "gold";
-            case "copper_ingot": return "copper";
-            case "netherite_ingot": return "netherite";
-            case "lapis_lazuli": return "lapis";
-            case "glowstone_dust": return "glowstone";
-            case "amethyst_shard": return "amethyst";
-            default: return id;
-        }
+        Bukkit.getLogger().info("[Bridge] Prix update -> " + item + " = " + price);
     }
 }

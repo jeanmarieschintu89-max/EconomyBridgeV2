3deacdfe1732bb65f6f912dc0642b706575b0953
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-04-27 22:25:01 +0200

    Update Main.java

--- src/main/java/fr/moodcraft/bridge/Main.java
+++ src/main/java/fr/moodcraft/bridge/Main.java
@@ -8,24 +8,18 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        instance = this;
 
-        PriceUpdater.init(this);
+        instance = this;
 
         // Listener QuickShop
         getServer().getPluginManager().registerEvents(new ShopListener(), this);
 
-        // Commande pour MAJ des prix QuickShop
+        // Commande update prix
         if (getCommand("priceupdate") != null) {
             getCommand("priceupdate").setExecutor(new PriceCommand());
         }
 
         getLogger().info("EconomyBridgeV2 activé !");
-    }
-
-    @Override
-    public void onDisable() {
-        getLogger().info("EconomyBridgeV2 désactivé !");
     }
 
     public static Main getInstance() {

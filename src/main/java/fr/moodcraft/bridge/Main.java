ac974fb80d08bd3cb71dfd61a0875a0247ef7e1e
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-04-27 17:58:45 +0200

    Update Main.java

--- src/main/java/fr/moodcraft/bridge/Main.java
+++ src/main/java/fr/moodcraft/bridge/Main.java
@@ -2,34 +2,16 @@ package fr.moodcraft.bridge;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-
 public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-
-        getLogger().info("EconomyBridge activé");
-
-        // crée dossier
-        if (!getDataFolder().exists()) {
-            getDataFolder().mkdirs();
-        }
-
-        // crée fichier data.yml
-        File file = new File(getDataFolder(), "data.yml");
-        if (!file.exists()) {
-            saveResource("data.yml", false);
-        }
-
-        // listener achat
-        getServer().getPluginManager().registerEvents(new ShopListener(this), this);
-
-        getLogger().info("Bridge prêt");
+        getServer().getPluginManager().registerEvents(new ShopListener(), this);
+        getLogger().info("EconomyBridge activé !");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("EconomyBridge désactivé");
+        getLogger().info("EconomyBridge désactivé !");
     }
 }

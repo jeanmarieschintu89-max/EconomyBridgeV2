f012bca18e05edefd3cb3afd730c942e10a125ac
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-04-27 22:55:27 +0200

    Update PriceCommand.java

--- src/main/java/fr/moodcraft/bridge/PriceCommand.java
+++ src/main/java/fr/moodcraft/bridge/PriceCommand.java
@@ -1,45 +1,23 @@
 package fr.moodcraft.bridge;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import com.ghostchu.quickshop.api.shop.Shop;
+import com.ghostchu.quickshop.api.QuickShopAPI;
 
 public class PriceCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-        if (args.length < 2) {
-            sender.sendMessage("Usage: /priceupdate <item> <price>");
+        if (Bukkit.getServicesManager().load(QuickShopAPI.class) == null) {
+            sender.sendMessage("§cQuickShop pas encore prêt !");
             return true;
         }
 
-        String item = args[0].toUpperCase();
-        double price;
-
-        try {
-            price = Double.parseDouble(args[1]);
-        } catch (Exception e) {
-            sender.sendMessage("Prix invalide");
-            return true;
-        }
-
-        for (Shop shop : Main.getInstance().getServer()
-                .getServicesManager()
-                .load(com.ghostchu.quickshop.api.QuickShopAPI.class)
-                .getShopManager()
-                .getAllShops()) {
-
-            String shopItem = shop.getItem().getType().name();
-
-            if (shopItem.contains(item)) {
-                shop.setPrice(price);
-                shop.update();
-            }
-        }
-
+        sender.sendMessage("§aQuickShop OK !");
         return true;
     }
 }

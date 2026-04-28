626b46f33e3ca40fb4473d28601b8abb6bc2982c
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-04-28 02:26:41 +0200

    Update ShopListener.java

--- src/main/java/fr/moodcraft/bridge/ShopListener.java
+++ src/main/java/fr/moodcraft/bridge/ShopListener.java
@@ -4,32 +4,49 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import com.ghostchu.quickshop.api.event.ShopPurchaseEvent;
+import com.ghostchu.quickshop.api.event.ShopSuccessPurchaseEvent;
 
 public class ShopListener implements Listener {
 
     @EventHandler
-    public void onBuy(ShopPurchaseEvent e) {
+    public void onBuy(ShopSuccessPurchaseEvent e) {
 
-        String item = e.getItem().getType().name().toLowerCase();
+        // 📦 Récupération item + quantité
+        String item = e.getShop().getItem().getType().name().toLowerCase();
         int amount = e.getAmount();
 
-        // 🔄 conversion noms Minecraft → ton système
-        if (item.equals("iron_ingot")) item = "iron";
-        if (item.equals("gold_ingot")) item = "gold";
-        if (item.equals("copper_ingot")) item = "copper";
-        if (item.equals("lapis_lazuli")) item = "lapis";
-        if (item.equals("glowstone_dust")) item = "glowstone";
-        if (item.equals("amethyst_shard")) item = "amethyst";
-        if (item.equals("netherite_ingot")) item = "netherite";
+        // 🔄 Conversion vers ton système Skript
+        switch (item) {
+            case "iron_ingot":
+                item = "iron";
+                break;
+            case "gold_ingot":
+                item = "gold";
+                break;
+            case "copper_ingot":
+                item = "copper";
+                break;
+            case "lapis_lazuli":
+                item = "lapis";
+                break;
+            case "glowstone_dust":
+                item = "glowstone";
+                break;
+            case "amethyst_shard":
+                item = "amethyst";
+                break;
+            case "netherite_ingot":
+                item = "netherite";
+                break;
+        }
 
-        // 🧪 DEBUG
-        Bukkit.getLogger().info("[EcoBridge] eco_buy " + item + " " + amount);
+        // 📊 Debug console
+        Bukkit.getLogger().info("[EcoBridge] Achat détecté: " + item + " x" + amount);
 
-        // ⚡ envoi vers Skript
+        // 🔌 Envoi vers Skript
         Bukkit.dispatchCommand(
-            Bukkit.getConsoleSender(),
-            "eco_buy " + item + " " + amount
+                Bukkit.getConsoleSender(),
+                "eco_buy " + item + " " + amount
         );
     }
 }

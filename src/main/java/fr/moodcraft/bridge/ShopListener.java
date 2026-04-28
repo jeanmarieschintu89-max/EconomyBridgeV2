d879e3ce5405c5e7f4c1069ed28b6be8d132bce0
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-04-28 03:03:27 +0200

    Update ShopListener.java

--- src/main/java/fr/moodcraft/bridge/ShopListener.java
+++ src/main/java/fr/moodcraft/bridge/ShopListener.java
@@ -1,52 +1,73 @@
-package fr.moodcraft.bridge;
+@EventHandler
+public void onBuy(ShopPurchaseEvent e) {
 
-import org.bukkit.Bukkit;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+    String item = e.getItem().getType().name().toLowerCase();
+    int amount = e.getAmount();
 
-import com.ghostchu.quickshop.api.event.ShopSuccessPurchaseEvent;
+    // 🔄 NORMALISATION
+    switch (item) {
+        case "iron_ingot":
+        case "raw_iron":
+            item = "iron";
+            break;
 
-public class ShopListener implements Listener {
+        case "gold_ingot":
+        case "raw_gold":
+            item = "gold";
+            break;
 
-    @EventHandler
-    public void onBuy(ShopSuccessPurchaseEvent e) {
+        case "copper_ingot":
+        case "raw_copper":
+            item = "copper";
+            break;
 
-        // 📦 Récupération item + quantité
-        String item = e.getShop().getItem().getType().name().toLowerCase();
-        int amount = e.getAmount();
+        case "lapis_lazuli":
+            item = "lapis";
+            break;
 
-        // 🔄 Conversion vers ton système Skript
-        switch (item) {
-            case "iron_ingot":
-                item = "iron";
-                break;
-            case "gold_ingot":
-                item = "gold";
-                break;
-            case "copper_ingot":
-                item = "copper";
-                break;
-            case "lapis_lazuli":
-                item = "lapis";
-                break;
-            case "glowstone_dust":
-                item = "glowstone";
-                break;
-            case "amethyst_shard":
-                item = "amethyst";
-                break;
-            case "netherite_ingot":
-                item = "netherite";
-                break;
-        }
+        case "redstone":
+            item = "redstone";
+            break;
 
-        // 📊 Debug console
-        Bukkit.getLogger().info("[EcoBridge] Achat détecté: " + item + " x" + amount);
+        case "diamond":
+            item = "diamond";
+            break;
 
-        // 🔌 Envoi vers Skript
-        Bukkit.dispatchCommand(
-                Bukkit.getConsoleSender(),
-                "eco_buy " + item + " " + amount
-        );
+        case "emerald":
+            item = "emerald";
+            break;
+
+        case "coal":
+        case "charcoal":
+            item = "coal";
+            break;
+
+        case "quartz":
+            item = "quartz";
+            break;
+
+        case "glowstone_dust":
+        case "glowstone":
+            item = "glowstone";
+            break;
+
+        case "amethyst_shard":
+            item = "amethyst";
+            break;
+
+        case "netherite_ingot":
+            item = "netherite";
+            break;
+
+        default:
+            Bukkit.getLogger().warning("[EcoBridge] Item ignoré: " + item);
+            return;
     }
+
+    Bukkit.getLogger().info("[EcoBridge] eco_buy " + item + " " + amount);
+
+    Bukkit.dispatchCommand(
+        Bukkit.getConsoleSender(),
+        "eco_buy " + item + " " + amount
+    );
 }

a01b9d9db957d44277882468b697365803250981
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-04-29 21:06:00 +0200

    Update ShopListener.java

--- src/main/java/fr/moodcraft/bridge/ShopListener.java
+++ src/main/java/fr/moodcraft/bridge/ShopListener.java
@@ -38,21 +38,22 @@ public class ShopListener implements Listener {
 
     private String normalize(String mat) {
 
-    switch (mat) {
-        case "diamond": return "diamond";
-        case "emerald": return "emerald";
-        case "gold_ingot": return "gold";
-        case "iron_ingot": return "iron";
-        case "copper_ingot": return "copper";
-        case "coal":
-        case "charcoal": return "coal";
-        case "lapis_lazuli": return "lapis";
-        case "redstone": return "redstone";
-        case "quartz": return "quartz";
-        case "amethyst_shard": return "amethyst";
-        case "netherite_ingot": return "netherite";
-        case "glowstone_dust": return "glowstone";
-    }
+        switch (mat) {
+            case "diamond": return "diamond";
+            case "emerald": return "emerald";
+            case "gold_ingot": return "gold";
+            case "iron_ingot": return "iron";
+            case "copper_ingot": return "copper";
+            case "coal":
+            case "charcoal": return "coal";
+            case "lapis_lazuli": return "lapis";
+            case "redstone": return "redstone";
+            case "quartz": return "quartz";
+            case "amethyst_shard": return "amethyst";
+            case "netherite_ingot": return "netherite";
+            case "glowstone_dust": return "glowstone";
+        }
 
-    return mat;
+        return mat;
+    }
 }

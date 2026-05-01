c782796c7ed658c8754bc34d096c7d2494c16ed6
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-01 23:37:59 +0200

    Create ContractManager.java

---
+++ src/main/java/fr/moodcraft/bridge/ContractManager.java
@@ -0,0 +1,31 @@
+package fr.moodcraft.bridge;
+
+import java.util.*;
+
+public class ContractManager {
+
+    public static Map<UUID, Contract> contracts = new HashMap<>();
+
+    public static class Contract {
+        public String from;
+        public String to;
+        public String item;
+        public int amount;
+        public double price;
+        public boolean accepted = false;
+
+        public Contract(String from, String to, String item, int amount, double price) {
+            this.from = from;
+            this.to = to;
+            this.item = item;
+            this.amount = amount;
+            this.price = price;
+        }
+    }
+
+    public static UUID create(String from, String to, String item, int amount, double price) {
+        UUID id = UUID.randomUUID();
+        contracts.put(id, new Contract(from, to, item, amount, price));
+        return id;
+    }
+}

05d6c3e1f71839b6730bb22a34d2a3ca1ea8c40f
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-01 13:57:19 +0200

    Update ItemNormalizer.java

--- src/main/java/fr/moodcraft/bridge/ItemNormalizer.java
+++ src/main/java/fr/moodcraft/bridge/ItemNormalizer.java
@@ -1,19 +1,42 @@
 package fr.moodcraft.bridge;
 
-public final class ItemNormalizer {
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 
-    public static String normalize(String mat) {
+public class ItemNormalizer {
 
-        switch (mat) {
-            case "gold_ingot": return "gold";
-            case "iron_ingot": return "iron";
-            case "copper_ingot": return "copper";
-            case "lapis_lazuli": return "lapis";
-            case "amethyst_shard": return "amethyst";
-            case "netherite_ingot": return "netherite";
-            case "glowstone_dust": return "glowstone";
-        }
+    public static String normalize(Material mat) {
+        return switch (mat) {
+            case DIAMOND -> "diamond";
+            case EMERALD -> "emerald";
+            case GOLD_INGOT -> "gold";
+            case IRON_INGOT -> "iron";
+            case COPPER_INGOT -> "copper";
+            case REDSTONE -> "redstone";
+            case LAPIS_LAZULI -> "lapis";
+            case COAL, CHARCOAL -> "coal";
+            case QUARTZ -> "quartz";
+            case NETHERITE_INGOT -> "netherite";
+            case AMETHYST_SHARD -> "amethyst";
+            case GLOWSTONE_DUST -> "glowstone";
+            default -> null;
+        };
+    }
 
-        return mat;
+    public static String normalizeBlock(Block block) {
+        return switch (block.getType()) {
+            case DIAMOND_ORE, DEEPSLATE_DIAMOND_ORE -> "diamond";
+            case EMERALD_ORE, DEEPSLATE_EMERALD_ORE -> "emerald";
+            case GOLD_ORE, DEEPSLATE_GOLD_ORE -> "gold";
+            case IRON_ORE, DEEPSLATE_IRON_ORE -> "iron";
+            case COPPER_ORE, DEEPSLATE_COPPER_ORE -> "copper";
+            case REDSTONE_ORE, DEEPSLATE_REDSTONE_ORE -> "redstone";
+            case LAPIS_ORE, DEEPSLATE_LAPIS_ORE -> "lapis";
+            case COAL_ORE, DEEPSLATE_COAL_ORE -> "coal";
+            case NETHER_QUARTZ_ORE -> "quartz";
+            case GLOWSTONE -> "glowstone";
+            case AMETHYST_CLUSTER -> "amethyst";
+            default -> null;
+        };
     }
 }

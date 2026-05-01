185b4866d5ccaec154631c2266851495b459e808
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-01 19:35:59 +0200

    Create MenuCommand.java

---
+++ src/main/java/fr/moodcraft/bridge/MenuCommand.java
@@ -0,0 +1,21 @@
+package fr.moodcraft.bridge;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class MenuCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player p)) {
+            sender.sendMessage("Commande joueur uniquement.");
+            return true;
+        }
+
+        MainMenuGUI.open(p);
+        return true;
+    }
+}

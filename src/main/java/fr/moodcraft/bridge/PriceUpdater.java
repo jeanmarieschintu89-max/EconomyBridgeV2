24860a8bef177eac29429b20de9283650a9a09d0
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-01 14:17:40 +0200

    Update PriceUpdater.java

--- src/main/java/fr/moodcraft/bridge/PriceUpdater.java
+++ src/main/java/fr/moodcraft/bridge/PriceUpdater.java
@@ -1,9 +1,7 @@
 package fr.moodcraft.bridge;
 
 import com.ghostchu.quickshop.api.shop.Shop;
-import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.Iterator;
 import java.util.Set;
 
 public final class PriceUpdater {
@@ -23,29 +21,15 @@ public final class PriceUpdater {
         Set<Shop> shops = ShopIndex.get(item);
         if (shops == null || shops.isEmpty()) return;
 
-        Iterator<Shop> it = shops.iterator();
+        // ⚡ UPDATE INSTANTANÉ
+        for (Shop shop : shops) {
 
-        new BukkitRunnable() {
-            @Override
-            public void run() {
+            if (shop == null) continue;
 
-                int count = 0;
-
-                while (it.hasNext() && count < 25) {
-                    Shop shop = it.next();
-
-                    if (shop == null) continue;
-
-                    if (Math.abs(shop.getPrice() - price) > 0.01) {
-                        shop.setPrice(price);
-                    }
-
-                    count++;
-                }
-
-                if (!it.hasNext()) cancel();
+            if (Math.abs(shop.getPrice() - price) > 0.01) {
+                shop.setPrice(price);
             }
-        }.runTaskTimer(Main.getInstance(), 0L, 1L);
+        }
     }
 
     public static void updateSingle(Shop shop, String item) {

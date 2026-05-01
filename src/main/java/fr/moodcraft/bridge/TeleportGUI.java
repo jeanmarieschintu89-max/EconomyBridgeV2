0fa943be0b4f5c82482fc431916219ba6f778424
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-01 21:59:41 +0200

    Update TeleportGUI.java

--- src/main/java/fr/moodcraft/bridge/TeleportGUI.java
+++ src/main/java/fr/moodcraft/bridge/TeleportGUI.java
@@ -13,32 +13,28 @@ public class TeleportGUI {
 
         // 🌲 Ressources
         inv.setItem(10, ItemBuilder.of(Material.OAK_LOG, "§a🌲 Ressources",
-                "§7Aller miner et récolter",
-                "",
                 "§8Clique pour tp"));
 
         // 🛒 Shop
         inv.setItem(11, ItemBuilder.of(Material.EMERALD, "§6🛒 Shop",
-                "§7Marché du serveur",
+                "§8Clique pour tp"));
+
+        // 🎲 TPR
+        inv.setItem(13, ItemBuilder.of(Material.ENDER_EYE, "§d🎲 Téléportation aléatoire",
+                "§7Exploration libre",
                 "",
                 "§8Clique pour tp"));
 
         // 🎮 Mini-jeux
         inv.setItem(12, ItemBuilder.of(Material.DIAMOND_SWORD, "§d🎮 Mini-jeux",
-                "§7Zone fun",
-                "",
                 "§8Clique pour tp"));
 
         // 🏠 Spawn
         inv.setItem(14, ItemBuilder.of(Material.COMPASS, "§e🏠 Spawn",
-                "§7Retour au spawn",
-                "",
                 "§8Clique pour tp"));
 
         // 🏙️ Ville
         inv.setItem(15, ItemBuilder.of(Material.BRICKS, "§a🏙️ Ville",
-                "§7Retour à ta ville",
-                "",
                 "§8Clique pour tp"));
 
         // 🔙 Retour

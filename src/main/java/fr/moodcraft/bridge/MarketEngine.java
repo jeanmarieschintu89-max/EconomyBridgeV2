d43fe44998580a982a8583810ded3de01dcccd82
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-01 05:29:36 +0200

    Update MarketEngine.java

--- src/main/java/fr/moodcraft/bridge/MarketEngine.java
+++ src/main/java/fr/moodcraft/bridge/MarketEngine.java
@@ -1,33 +1,6 @@
 package fr.moodcraft.bridge;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MarketEngine {
-
-    private static final Map<String, Double> stock = new HashMap<>();
-
-    public static void buy(String item, double amount) {
-
-        if (!PriceUpdater.ALLOWED.contains(item)) return;
-
-        double price = getPrice(item);
-
-        double impact = (amount * price) / 1000;
-
-        double weight = Main.getInstance().getConfig().getDouble("weight." + item, 1);
-        double div = Main.getInstance().getConfig().getDouble("impact." + item, 40);
-
-        impact = impact * weight;
-
-        if (impact > 2) impact = 2;
-
-        double newPrice = price + (impact / div);
-
-        setPrice(item, newPrice);
-
-        PriceUpdater.updateItem(item);
-    }
 
     public static void sell(String item, double amount) {
 
@@ -40,18 +13,42 @@ public class MarketEngine {
         double weight = Main.getInstance().getConfig().getDouble("weight." + item, 1);
         double div = Main.getInstance().getConfig().getDouble("impact." + item, 40);
 
-        impact = impact * weight;
+        impact *= weight;
 
         if (impact > 2) impact = 2;
 
-        impact = impact * -1;
+        impact *= -1;
 
         double newPrice = price + (impact / div);
 
         setPrice(item, newPrice);
 
         // 📦 stock
-        stock.put(item, stock.getOrDefault(item, 0.0) + amount);
+        MarketState.stock.put(item,
+                MarketState.stock.getOrDefault(item, 0.0) + amount
+        );
+
+        PriceUpdater.updateItem(item);
+    }
+
+    public static void buy(String item, double amount) {
+
+        if (!PriceUpdater.ALLOWED.contains(item)) return;
+
+        double price = getPrice(item);
+
+        double impact = (amount * price) / 1000;
+
+        double weight = Main.getInstance().getConfig().getDouble("weight." + item, 1);
+        double div = Main.getInstance().getConfig().getDouble("impact." + item, 40);
+
+        impact *= weight;
+
+        if (impact > 2) impact = 2;
+
+        double newPrice = price + (impact / div);
+
+        setPrice(item, newPrice);
 
         PriceUpdater.updateItem(item);
     }

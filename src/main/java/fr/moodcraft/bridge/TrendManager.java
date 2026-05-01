8760dee4aaa5cd4a6c46236897a634e70e49270f
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-01 05:31:09 +0200

    Create src/main/java/fr/moodcraft/bridge/TrendManager.java

---
+++ src/main/java/fr/moodcraft/bridge/TrendManager.java
@@ -0,0 +1,54 @@
+package fr.moodcraft.bridge;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class TrendManager {
+
+    public static void updateTrend(String item, double newPrice) {
+
+        double old = MarketState.lastPrice.getOrDefault(item, newPrice);
+
+        double diff = newPrice - old;
+
+        double percent = (old == 0) ? 0 : (diff / old) * 100;
+
+        diff = round(diff);
+        percent = round(percent);
+
+        String result;
+
+        if (diff > 0) {
+
+            if (percent > 5) {
+                result = "§2⬆ +" + percent + "%";
+            } else {
+                result = "§a⬆ +" + percent + "%";
+            }
+
+        } else if (diff < 0) {
+
+            if (percent < -5) {
+                result = "§4⬇ " + percent + "%";
+            } else {
+                result = "§c⬇ " + percent + "%";
+            }
+
+        } else {
+            result = "§7➡ stable";
+        }
+
+        MarketState.trend.put(item, result);
+        MarketState.lastPrice.put(item, newPrice);
+    }
+
+    public static String getTrend(String item) {
+        return MarketState.trend.getOrDefault(item, "§7➡ stable");
+    }
+
+    private static double round(double v) {
+        return new BigDecimal(v)
+                .setScale(2, RoundingMode.HALF_UP)
+                .doubleValue();
+    }
+}

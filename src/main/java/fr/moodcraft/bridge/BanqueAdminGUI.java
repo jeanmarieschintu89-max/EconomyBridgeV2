5e30d7b148fa456af6a604201db46587c9d23bc8
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-01 17:37:34 +0200

    Update BanqueAdminGUI.java

--- src/main/java/fr/moodcraft/bridge/BanqueAdminGUI.java
+++ src/main/java/fr/moodcraft/bridge/BanqueAdminGUI.java
@@ -15,8 +15,12 @@ public class BanqueAdminGUI {
 
         inv.setItem(1, item(Material.EMERALD_BLOCK, "§aInflation +5%"));
         inv.setItem(3, item(Material.REDSTONE_BLOCK, "§cDéflation -5%"));
-        inv.setItem(4, item(Material.BEACON, "§bReload Économie")); // ✔ propre
+        inv.setItem(4, item(Material.BEACON, "§bReload Économie"));
         inv.setItem(5, item(Material.NETHER_STAR, "§eSynchroniser"));
+
+        // 🔥 NOUVEAU BOUTON CONFIG
+        inv.setItem(6, item(Material.COMPARATOR, "§dConfigurer Marché"));
+
         inv.setItem(7, item(Material.BARRIER, "§4Reset Économie"));
 
         p.openInventory(inv);

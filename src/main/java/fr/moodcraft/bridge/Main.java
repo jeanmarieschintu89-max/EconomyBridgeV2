18d4e0b3fc502d1f493c45f77b8a28c2f0c79664
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-01 13:54:48 +0200

    Update Main.java

--- src/main/java/fr/moodcraft/bridge/Main.java
+++ src/main/java/fr/moodcraft/bridge/Main.java
@@ -9,26 +9,41 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
+
         instance = this;
 
-        getLogger().info("🚀 EconomyBridge démarrage...");
-
-        saveDefaultConfig();
-
-        // 🔌 Listeners
         Bukkit.getPluginManager().registerEvents(new ShopListener(), this);
+        Bukkit.getPluginManager().registerEvents(new MineListener(), this);
+        Bukkit.getPluginManager().registerEvents(new GUIListener(), this);
 
-        // 🔄 Index
+        getCommand("prix").setExecutor(new PrixCommand());
+        getCommand("syncprix").setExecutor(new SyncCommand());
+        getCommand("trend").setExecutor(new GetTrendCommand());
+
+        initBase();
+
+        Bukkit.getScheduler().runTaskTimer(this, MarketEngine::tick, 20*45, 20*45);
+
         ShopIndex.rebuild();
+    }
 
-        Bukkit.getScheduler().runTaskTimer(this, ShopIndex::rebuild, 20L * 60, 20L * 60);
+    private void initBase() {
 
-        // 📜 Commande priceupdate
-        if (getCommand("priceupdate") != null) {
-            getCommand("priceupdate").setExecutor(new PriceCommand());
-        }
+        MarketState.base.put("diamond", 2800.0);
+        MarketState.base.put("emerald", 15000.0);
+        MarketState.base.put("netherite", 32000.0);
 
-        getLogger().info("✅ Plugin actif");
+        MarketState.base.put("gold", 120.0);
+        MarketState.base.put("iron", 80.0);
+        MarketState.base.put("copper", 12.0);
+
+        MarketState.base.put("redstone", 20.0);
+        MarketState.base.put("lapis", 20.0);
+        MarketState.base.put("coal", 8.0);
+        MarketState.base.put("quartz", 25.0);
+
+        MarketState.base.put("glowstone", 15.0);
+        MarketState.base.put("amethyst", 6.0);
     }
 
     public static Main getInstance() {

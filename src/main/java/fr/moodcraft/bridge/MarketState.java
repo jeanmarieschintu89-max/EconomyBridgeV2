87db15f18301492ca761ebc71df2b843ab57f357
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-01 05:30:40 +0200

    Update MarketState.java

--- src/main/java/fr/moodcraft/bridge/MarketState.java
+++ src/main/java/fr/moodcraft/bridge/MarketState.java
@@ -6,6 +6,11 @@ import java.util.Map;
 public class MarketState {
 
     public static final Map<String, Double> stock = new HashMap<>();
+
+    // 💎 ancien prix (pour trend)
     public static final Map<String, Double> lastPrice = new HashMap<>();
+
+    // 📈 texte trend prêt à afficher
+    public static final Map<String, String> trend = new HashMap<>();
 
 }

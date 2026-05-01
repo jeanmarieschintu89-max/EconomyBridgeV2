830aeea3974d3589499fd3a7750a301f5eda0ebc
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-02 00:47:05 +0200

    Update ContractManager.java

--- src/main/java/fr/moodcraft/bridge/ContractManager.java
+++ src/main/java/fr/moodcraft/bridge/ContractManager.java
@@ -12,6 +12,8 @@ public class ContractManager {
         public String item;
         public int amount;
         public double price;
+        public boolean accepted;
+public boolean signed = false; // 🔥 AJOUT
         public boolean accepted = false;
 
         public Contract(String from, String to, String item, int amount, double price) {

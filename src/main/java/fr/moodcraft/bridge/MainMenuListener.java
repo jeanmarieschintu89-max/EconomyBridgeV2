a016835e12fdb9b5e17e8ecdfb243105fad5895f
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-01 19:52:10 +0200

    Update MainMenuListener.java

--- src/main/java/fr/moodcraft/bridge/MainMenuListener.java
+++ src/main/java/fr/moodcraft/bridge/MainMenuListener.java
@@ -35,7 +35,7 @@ public class MainMenuListener implements Listener {
 
             case 11 -> { // 🏙️ Ville
                 p.closeInventory();
-                p.performCommand("town");
+                p.performCommand("town menu");
             }
 
             case 12 -> { // ⚒️ Jobs

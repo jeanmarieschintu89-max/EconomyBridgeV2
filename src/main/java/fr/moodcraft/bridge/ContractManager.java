42b489e4bbfa3b0ca21ed6e0f8d06b28280733ca
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-02 00:54:38 +0200

    Update ContractManager.java

--- src/main/java/fr/moodcraft/bridge/ContractManager.java
+++ src/main/java/fr/moodcraft/bridge/ContractManager.java
@@ -1,20 +1,23 @@
 package fr.moodcraft.bridge;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class ContractManager {
 
     public static Map<UUID, Contract> contracts = new HashMap<>();
 
     public static class Contract {
+
         public String from;
         public String to;
         public String item;
         public int amount;
         public double price;
-        public boolean accepted;
-public boolean signed = false; // 🔥 AJOUT
-        public boolean accepted = false;
+
+        public boolean accepted = false; // ✔ UNE SEULE FOIS
+        public boolean signed = false;   // ✔ signature
 
         public Contract(String from, String to, String item, int amount, double price) {
             this.from = from;

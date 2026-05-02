0c2e6514bff435ead0a56676667dc3c5ceb2d7be
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 00:23:24 +0200

    Update ContractGUI.java

--- src/main/java/fr/moodcraft/bridge/ContractGUI.java
+++ src/main/java/fr/moodcraft/bridge/ContractGUI.java
@@ -11,46 +11,28 @@ public class ContractGUI {
 
         Inventory inv = Bukkit.createInventory(null, 27, "§e📜 Contrats");
 
-        // 📄 CRÉER
         SafeGUI.safeSet(inv, 10, SafeGUI.item(Material.WRITABLE_BOOK,
                 "§aCréer un contrat",
-                "§8────────────",
-                "§7Créer une demande",
-                "",
-                "§e/contract <item> <quantité> <prix>"
+                "§7/contract <item> <quantité> <prix>"
         ));
 
-        // ✔ ACCEPTER
         SafeGUI.safeSet(inv, 12, SafeGUI.item(Material.EMERALD,
                 "§aAccepter contrat",
-                "§8────────────",
-                "§7Accepter un contrat",
-                "",
-                "§e/contractaccept"
+                "§7/contractaccept"
         ));
 
-        // 📦 LIVRER
         SafeGUI.safeSet(inv, 14, SafeGUI.item(Material.CHEST,
                 "§bLivrer contrat",
-                "§8────────────",
-                "§7Livrer les items",
-                "",
-                "§e/contractdeliver"
+                "§7/contractdeliver"
         ));
 
-        // 📜 LOG
         SafeGUI.safeSet(inv, 16, SafeGUI.item(Material.BOOK,
                 "§dHistorique",
-                "§8────────────",
-                "§7Voir les contrats",
-                "",
-                "§e/contractlog"
+                "§7/contractlog"
         ));
 
-        // 🔙 RETOUR
         SafeGUI.safeSet(inv, 22, SafeGUI.item(Material.BARRIER,
-                "§cRetour",
-                "§7Menu principal"));
+                "§cRetour"));
 
         p.openInventory(inv);
     }

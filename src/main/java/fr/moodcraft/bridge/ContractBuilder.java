629e0fa0b104255d18470cf93562e973dbbf1fc2
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-02 15:19:52 +0200

    Update ContractBuilder.java

--- src/main/java/fr/moodcraft/bridge/ContractBuilder.java
+++ src/main/java/fr/moodcraft/bridge/ContractBuilder.java
@@ -1,24 +1,30 @@
 package fr.moodcraft.bridge;
 
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class ContractBuilder {
 
     public String target;
     public String item;
+
+    public ItemStack itemStack; // 🔥 AJOUT IMPORTANT
+
     public int amount = 1;
     public double price = 100;
 
-    private static final Map<Player, ContractBuilder> map = new HashMap<>();
+    // 🔥 UUID au lieu de Player (évite memory leak)
+    private static final Map<UUID, ContractBuilder> map = new HashMap<>();
 
     public static ContractBuilder get(Player p) {
-        return map.computeIfAbsent(p, k -> new ContractBuilder());
+        return map.computeIfAbsent(p.getUniqueId(), k -> new ContractBuilder());
     }
 
     public static void remove(Player p) {
-        map.remove(p);
+        map.remove(p.getUniqueId());
     }
 }

4af136e19ccc8e021eb4bd759d0696593baa48b0
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-02 04:11:28 +0200

    Update ChatListener.java

--- src/main/java/fr/moodcraft/bridge/ChatListener.java
+++ src/main/java/fr/moodcraft/bridge/ChatListener.java
@@ -1,18 +1,26 @@
 package fr.moodcraft.bridge;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class ChatListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void chat(AsyncPlayerChatEvent e) {
+
+        if (e.isCancelled()) return;
 
         String player = e.getPlayer().getName();
 
+        // 🔥 sécurité null (au cas où)
         String rep = ReputationManager.format(player);
+        if (rep == null) rep = "§7?";
 
-        e.setFormat("§7[" + rep + "] §f" + player + " §8» §f" + e.getMessage());
+        // 🔥 format propre
+        String message = e.getMessage();
+
+        e.setFormat("§7[" + rep + "] §f" + player + " §8» §f" + message);
     }
 }

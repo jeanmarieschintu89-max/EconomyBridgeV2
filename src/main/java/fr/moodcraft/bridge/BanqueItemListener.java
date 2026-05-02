8e485cba26481435c0c61da873a3c1bc43e40891
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-02 04:36:48 +0200

    Update BanqueItemListener.java

--- src/main/java/fr/moodcraft/bridge/BanqueItemListener.java
+++ src/main/java/fr/moodcraft/bridge/BanqueItemListener.java
@@ -1,6 +1,7 @@
 package fr.moodcraft.bridge;
 
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,39 +15,36 @@ public class BanqueItemListener implements Listener {
 
         Player p = e.getPlayer();
 
-        if (e.getItem() == null) return;
-
         ItemStack item = e.getItem();
-
-        if (item.getType() == Material.AIR) return;
-        if (!item.hasItemMeta()) return;
-        if (!item.getItemMeta().hasDisplayName()) return;
+        if (item == null || item.getType() == Material.AIR) return;
+        if (!item.hasItemMeta() || !item.getItemMeta().hasDisplayName()) return;
 
         String name = item.getItemMeta().getDisplayName();
 
         // =========================
         // 💳 CARTE BANCAIRE
         // =========================
-        if (name.equalsIgnoreCase("§b💳 Carte bancaire")) {
+        if (name.contains("Carte")) {
 
             e.setCancelled(true);
 
             BankGUI.open(p);
 
-            p.playSound(p.getLocation(), "ui.button.click", 1f, 1f);
+            // 🔥 SON SAFE
+            p.playSound(p.getLocation(), Sound.UI_BUTTON_CLICK, 1f, 1f);
         }
 
         // =========================
         // 📤 IBAN
         // =========================
-        if (name.equalsIgnoreCase("§e📤 IBAN")) {
+        else if (name.contains("IBAN")) {
 
             e.setCancelled(true);
 
             String iban = BankStorage.getIban(p.getUniqueId().toString());
 
             p.sendMessage("§8━━━━━━━━━━━━━━━━━━");
-            p.sendMessage("§e🏦 Banque MoodCraft");
+            p.sendMessage("§eBanque MoodCraft");
             p.sendMessage("§7Titulaire: §e" + p.getName());
             p.sendMessage("§7IBAN: §b" + iban);
             p.sendMessage("§8━━━━━━━━━━━━━━━━━━");

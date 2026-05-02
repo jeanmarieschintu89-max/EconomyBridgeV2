2b8b359146fdb07e47ddfc9cad6fc4cbfd3c322f
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-02 04:21:56 +0200

    Update PriceGUI.java

--- src/main/java/fr/moodcraft/bridge/PriceGUI.java
+++ src/main/java/fr/moodcraft/bridge/PriceGUI.java
@@ -17,8 +17,22 @@ public class PriceGUI {
             set(inv, p, 11, "emerald", Material.EMERALD, "§a◆ Émeraude");
             set(inv, p, 12, "diamond", Material.DIAMOND, "§b◆ Diamant");
 
+            set(inv, p, 13, "gold", Material.GOLD_INGOT, "§6◆ Or");
+            set(inv, p, 14, "copper", Material.COPPER_INGOT, "§6◆ Cuivre");
+            set(inv, p, 15, "iron", Material.IRON_INGOT, "§7◆ Fer");
+
+            set(inv, p, 16, "glowstone", Material.GLOWSTONE_DUST, "§e◆ Glowstone");
+
+            set(inv, p, 19, "quartz", Material.QUARTZ, "§f◆ Quartz");
+            set(inv, p, 20, "amethyst", Material.AMETHYST_SHARD, "§d◆ Améthyste");
+            set(inv, p, 21, "redstone", Material.REDSTONE, "§c◆ Redstone");
+            set(inv, p, 22, "lapis", Material.LAPIS_LAZULI, "§9◆ Lapis");
+            set(inv, p, 23, "coal", Material.COAL, "§8◆ Charbon");
+
         } catch (Exception e) {
             inv.clear();
+            SafeGUI.safeSet(inv, 13, SafeGUI.item(Material.BARRIER, "§cErreur Bourse"));
+            e.printStackTrace();
         }
 
         p.openInventory(inv);
@@ -31,13 +45,17 @@ public class PriceGUI {
 
         SafeGUI.safeSet(inv, slot,
                 SafeGUI.item(mat, name,
+
+                        "§8────────────",
                         "§7Prix:",
-                        "§f" + price + "€",
+                        "§f" + String.format("%.2f", price) + "€",
+                        "",
+                        "§7Tendance:",
+                        trend,
                         "",
                         "§7Reputation:",
                         ReputationManager.format(p.getName()),
-                        "",
-                        trend,
+                        "§8────────────",
                         "",
                         "§aClique pour vendre"));
     }

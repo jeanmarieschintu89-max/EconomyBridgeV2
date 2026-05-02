50fc54e4e728046f3a06df705b8515a2926fa1c5
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-02 19:26:25 +0200

    Update DebugListener.java

--- src/main/java/fr/moodcraft/bridge/DebugListener.java
+++ src/main/java/fr/moodcraft/bridge/DebugListener.java
@@ -1,19 +1,21 @@
 package fr.moodcraft.bridge;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class DebugListener implements Listener {
 
-    @EventHandler
-    public void debug(InventoryClickEvent e) {
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void low(InventoryClickEvent e) {
+        System.out.println("[LOWEST] cancelled=" + e.isCancelled() +
+                " | title=" + e.getView().getTitle());
+    }
 
-        if (e.isCancelled()) {
-            System.out.println("[DEBUG CANCEL] "
-                + e.getView().getTitle()
-                + " | raw=" + e.getRawSlot()
-                + " | inv=" + (e.getClickedInventory() == null ? "null" : e.getClickedInventory().getType()));
-        }
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void monitor(InventoryClickEvent e) {
+        System.out.println("[MONITOR] cancelled=" + e.isCancelled() +
+                " | title=" + e.getView().getTitle());
     }
 }

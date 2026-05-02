d169f0d8dd5a8d90c4b8198d9fb6accf2b87f37a
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-02 16:30:24 +0200

    Update BankTransferGUI.java

--- src/main/java/fr/moodcraft/bridge/BankTransferGUI.java
+++ src/main/java/fr/moodcraft/bridge/BankTransferGUI.java
@@ -9,25 +9,34 @@ public class BankTransferGUI {
 
     public static void open(Player p) {
 
-        Inventory inv = Bukkit.createInventory(null, 27, "§eVirement");
+        Inventory inv = Bukkit.createInventory(null, 27, "§fVirement");
 
+        // 💳 IBAN
         SafeGUI.safeSet(inv, 11, SafeGUI.item(Material.NAME_TAG, "§bPar IBAN",
-                "§8────────",
-                "§7Envoyer via IBAN",
+                "§8──────────── §7",
+                "§7Envoyer de l'argent §7",
+                "§7via un IBAN §7",
                 "",
-                "§8Clique"));
+                "§eCommande: §f/ibanpay §7",
+                "",
+                "§8Clique pour utiliser §7"));
 
+        // 👤 JOUEUR
         SafeGUI.safeSet(inv, 13, SafeGUI.item(Material.PLAYER_HEAD, "§aPar joueur",
-                "§8────────",
-                "§7Choisir un joueur",
+                "§8──────────── §7",
+                "§7Choisir un joueur §7",
+                "§7connecté §7",
                 "",
-                "§8Clique"));
+                "§aVirement rapide §7",
+                "",
+                "§8Clique pour sélectionner §7"));
 
+        // 🔙 RETOUR
         SafeGUI.safeSet(inv, 15, SafeGUI.item(Material.BARRIER, "§cRetour",
-                "§8────────",
-                "§7Retour à la banque",
+                "§8──────────── §7",
+                "§7Retour à la banque §7",
                 "",
-                "§8Clique"));
+                "§8Clique pour revenir §7"));
 
         p.openInventory(inv);
     }

2672395c0a069499f7797f980645f1002290c3f7
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 02:20:52 +0200

    Update BanqueItemListListener.java

--- src/main/java/fr/moodcraft/bridge/BanqueItemListListener.java
+++ src/main/java/fr/moodcraft/bridge/BanqueItemListListener.java
@@ -2,8 +2,7 @@ package fr.moodcraft.bridge;
 
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import org.bukkit.event.*;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
 public class BanqueItemListListener implements Listener {
@@ -18,21 +17,21 @@ public class BanqueItemListListener implements Listener {
 
         if (!clean.equalsIgnoreCase("Items Marché")) return;
 
+        if (!(e.getWhoClicked() instanceof Player p)) return;
         if (e.getClickedInventory() == null) return;
+
         if (e.getRawSlot() >= e.getView().getTopInventory().getSize()) return;
 
         e.setCancelled(true);
 
-        if (!(e.getWhoClicked() instanceof Player p)) return;
-
         var item = e.getCurrentItem();
         if (item == null || item.getType().isAir()) return;
 
-        String name = item.getItemMeta().getDisplayName();
-        String cleanItem = name.replace("§b", "").toLowerCase();
+        String name = item.getItemMeta().getDisplayName().replace("§b", "");
 
         p.playSound(p.getLocation(), Sound.UI_BUTTON_CLICK, 1f, 1.2f);
 
-        BanqueItemGUI.open(p, cleanItem);
+        // 🔥 OUVRE MENU ITEM
+        MarketItemGUI.open(p, name.toLowerCase());
     }
 }

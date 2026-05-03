27d9a79f85cb4573af9a18fffe0116150071864b
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 15:34:03 +0200

    Update TransferBuilder.java

--- src/main/java/fr/moodcraft/bridge/TransferBuilder.java
+++ src/main/java/fr/moodcraft/bridge/TransferBuilder.java
@@ -1,3 +1,11 @@
+package fr.moodcraft.bridge;
+
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class TransferBuilder {
 
     public UUID target;
@@ -6,7 +14,13 @@ public class TransferBuilder {
     private static final Map<UUID, TransferBuilder> map = new HashMap<>();
 
     public static TransferBuilder get(Player p) {
-        return map.computeIfAbsent(p.getUniqueId(), k -> new TransferBuilder());
+        return map.get(p.getUniqueId());
+    }
+
+    public static TransferBuilder create(Player p) {
+        TransferBuilder b = new TransferBuilder();
+        map.put(p.getUniqueId(), b);
+        return b;
     }
 
     public static void remove(Player p) {

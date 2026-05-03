0eb0d86ac174c41349eca88d62faf49aaa85fde4
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 14:32:32 +0200

    Create TargetPlayerHandler.java

---
+++ src/main/java/fr/moodcraft/bridge/TargetPlayerHandler.java
@@ -0,0 +1,22 @@
+package fr.moodcraft.bridge;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class TargetPlayerHandler implements GUIHandler {
+
+    @Override
+    public void onClick(Player p, int slot) {
+
+        Player target = Bukkit.getPlayerExact(p.getOpenInventory().getItem(slot).getItemMeta().getDisplayName());
+
+        if (target == null) {
+            p.sendMessage("§cJoueur invalide");
+            return;
+        }
+
+        TransferBuilder.get(p).target = target.getName();
+
+        TransferConfirmGUI.open(p);
+    }
+}

5e64a1d3a0a58bcc675ca3c504590d6368a73386
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 14:03:48 +0200

    Create ContractAmountListener.java

---
+++ src/main/java/fr/moodcraft/bridge/ContractAmountListener.java
@@ -0,0 +1,27 @@
+package fr.moodcraft.bridge;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class ContractAmountListener implements Listener {
+
+    @EventHandler
+    public void click(InventoryClickEvent e) {
+
+        String title = e.getView().getTitle();
+        if (title == null) return;
+
+        String clean = title.replaceAll("§.", "").trim();
+
+        if (!clean.equalsIgnoreCase("Quantité")) return;
+
+        if (!(e.getWhoClicked() instanceof Player p)) return;
+
+        if (e.getClickedInventory() == null) return;
+
+        if (e.getRawSlot() >= e.getView().getTopInventory().getSize()) return;
+
+        e.setCancelled(true);
+    }
+}

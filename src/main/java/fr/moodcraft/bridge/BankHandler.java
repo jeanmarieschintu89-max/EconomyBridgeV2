2e0421bb25776e8ec74ab4b06280677b9c9e405a
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 14:45:57 +0200

    Create BankHandler.java

---
+++ src/main/java/fr/moodcraft/bridge/BankHandler.java
@@ -0,0 +1,37 @@
+package fr.moodcraft.bridge;
+
+import org.bukkit.entity.Player;
+
+public class BankHandler implements GUIHandler {
+
+    @Override
+    public void onClick(Player p, int slot) {
+
+        switch (slot) {
+
+            case 10 -> {
+                // dépôt
+                p.sendMessage("§aDéposer bientôt...");
+            }
+
+            case 12 -> {
+                // retrait
+                p.sendMessage("§eRetirer bientôt...");
+            }
+
+            case 14 -> {
+                // virement
+                TransferTargetGUI.open(p);
+            }
+
+            case 16 -> {
+                // historique
+                BankHistoryGUI.open(p);
+            }
+
+            case 8 -> {
+                MainMenuGUI.open(p);
+            }
+        }
+    }
+}

1e5719b3ed98b4f34a0afe49d21d7b17fd19a1cc
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-04 00:00:46 +0200

    Create TopGUIHandler.java

---
+++ src/main/java/fr/moodcraft/bridge/TopGUIHandler.java
@@ -0,0 +1,34 @@
+package fr.moodcraft.bridge;
+
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.util.UUID;
+
+public class TopGUIHandler implements GUIHandler {
+
+    @Override
+    public void onClick(Player p, int slot) {
+
+        ItemStack item = p.getOpenInventory().getItem(slot);
+        if (item == null || !item.hasItemMeta()) return;
+
+        var meta = item.getItemMeta();
+
+        String uuidStr = meta.getPersistentDataContainer().get(
+                new NamespacedKey(Main.getInstance(), "target"),
+                PersistentDataType.STRING
+        );
+
+        if (uuidStr == null) return;
+
+        UUID targetUUID = UUID.fromString(uuidStr);
+
+        p.closeInventory();
+
+        // 👉 ouvre profil joueur
+        ProfileGUI.open(p, targetUUID);
+    }
+}

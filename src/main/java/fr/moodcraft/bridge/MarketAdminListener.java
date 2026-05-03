37019d0f7a5a5a8ea0a2d9a115b1e74a7f39db6e
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 02:16:55 +0200

    Create MarketAdminListener.java

---
+++ src/main/java/fr/moodcraft/bridge/MarketAdminListener.java
@@ -0,0 +1,30 @@
+package fr.moodcraft.bridge;
+
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class MarketAdminListener implements Listener {
+
+    @EventHandler
+    public void click(InventoryClickEvent e) {
+
+        String title = e.getView().getTitle();
+        if (title == null || !title.contains("Admin Marché")) return;
+
+        if (!(e.getWhoClicked() instanceof Player p)) return;
+        if (e.getClickedInventory() == null) return;
+
+        e.setCancelled(true);
+
+        int slot = e.getRawSlot();
+
+        p.playSound(p.getLocation(), Sound.UI_BUTTON_CLICK, 1f, 1.2f);
+
+        switch (slot) {
+            case 11 -> BanqueItemListGUI.open(p);
+            case 13 -> MarketGlobalGUI.open(p);
+        }
+    }
+}

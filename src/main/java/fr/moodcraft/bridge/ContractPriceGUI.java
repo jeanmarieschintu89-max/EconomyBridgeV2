1326ae44347c7240b6351da2f0d8c7bec8659a2c
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 03:51:31 +0200

    Create ContractPriceGUI.java

---
+++ src/main/java/fr/moodcraft/bridge/ContractPriceGUI.java
@@ -0,0 +1,30 @@
+package fr.moodcraft.bridge;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class ContractPriceGUI {
+
+    public static void open(Player p) {
+
+        ContractBuilder b = ContractBuilder.get(p.getUniqueId());
+
+        Inventory inv = Bukkit.createInventory(null, 27, "§fPrix");
+
+        SafeGUI.safeSet(inv, 10, SafeGUI.item(Material.RED_CONCRETE, "§c-100"));
+        SafeGUI.safeSet(inv, 11, SafeGUI.item(Material.RED_STAINED_GLASS, "§c-10"));
+
+        SafeGUI.safeSet(inv, 13, SafeGUI.item(Material.GOLD_INGOT,
+                "§ePrix actuel",
+                "§6" + b.price + "€"));
+
+        SafeGUI.safeSet(inv, 15, SafeGUI.item(Material.GREEN_STAINED_GLASS, "§a+10"));
+        SafeGUI.safeSet(inv, 16, SafeGUI.item(Material.LIME_CONCRETE, "§a+100"));
+
+        SafeGUI.safeSet(inv, 26, SafeGUI.item(Material.ARROW, "§cRetour"));
+
+        p.openInventory(inv);
+    }
+}

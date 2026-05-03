54f7c39f7a372df7d6bb750677f6ce29658cd62a
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 03:08:18 +0200

    Create BankTransferListener.java

---
+++ src/main/java/fr/moodcraft/bridge/BankTransferListener.java
@@ -0,0 +1,48 @@
+package fr.moodcraft.bridge;
+
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class BankTransferListener implements Listener {
+
+    @EventHandler
+    public void click(InventoryClickEvent e) {
+
+        String title = e.getView().getTitle();
+        if (title == null) return;
+
+        String clean = title.replaceAll("§.", "");
+        if (!clean.equalsIgnoreCase("Virement bancaire")) return;
+
+        if (!(e.getWhoClicked() instanceof Player p)) return;
+        if (e.getClickedInventory() == null) return;
+
+        if (e.getRawSlot() >= e.getView().getTopInventory().getSize()) return;
+
+        e.setCancelled(true);
+
+        int slot = e.getRawSlot();
+
+        p.playSound(p.getLocation(), Sound.UI_BUTTON_CLICK, 1f, 1.2f);
+
+        switch (slot) {
+
+            case 11 -> {
+                p.closeInventory();
+                p.performCommand("pay"); // ou ton GUI joueur
+            }
+
+            case 15 -> {
+                p.closeInventory();
+                p.performCommand("ibanpay");
+            }
+
+            case 22 -> {
+                p.closeInventory();
+                BankGUI.open(p);
+            }
+        }
+    }
+}

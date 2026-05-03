e8de0186033ebaa20189b00448ddfe9f0fd72934
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 17:44:01 +0200

    Update InventoryCloseListener.java

--- src/main/java/fr/moodcraft/bridge/InventoryCloseListener.java
+++ src/main/java/fr/moodcraft/bridge/InventoryCloseListener.java
@@ -1,6 +1,5 @@
 package fr.moodcraft.bridge;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.inventory.InventoryCloseEvent;
@@ -12,20 +11,6 @@ public class InventoryCloseListener implements Listener {
 
         if (!(e.getPlayer() instanceof Player p)) return;
 
-        String oldId = GUIManager.get(p);
-        if (oldId == null) return;
-
-        // ⏳ On attend 1 tick (sinon conflit entre anciens/nouveaux GUI)
-        Bukkit.getScheduler().runTask(Main.getInstance(), () -> {
-
-            String current = GUIManager.get(p);
-
-            // 🧠 Si c’est le même GUI → vrai close
-            if (oldId.equals(current)) {
-                GUIManager.close(p);
-            }
-
-            // 🔥 Sinon → un autre GUI a été ouvert → ON GARDE
-        });
+        GUIManager.close(p);
     }
 }

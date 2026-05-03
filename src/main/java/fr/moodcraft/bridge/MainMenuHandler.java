19a78a2334617079475e861a1d7af2ab33546778
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 22:08:46 +0200

    Update MainMenuHandler.java

--- src/main/java/fr/moodcraft/bridge/MainMenuHandler.java
+++ src/main/java/fr/moodcraft/bridge/MainMenuHandler.java
@@ -8,43 +8,16 @@ public class MainMenuHandler implements GUIHandler {
     @Override
     public void onClick(Player p, int slot) {
 
-        // 🔊 son clic
         p.playSound(p.getLocation(), Sound.UI_BUTTON_CLICK, 1f, 1f);
 
         switch (slot) {
 
-            case 4 -> p.sendMessage("§7Profil joueur");
-
-            case 10 -> {
-                p.closeInventory();
-                BankGUI.open(p);
-            }
-
-            case 12 -> {
-                p.closeInventory();
-                ContractGUI.open(p);
-            }
-
-            case 14 -> {
-                p.closeInventory();
-                PriceGUI.open(p);
-            }
-
-            case 16 -> {
-                p.closeInventory();
-                TeleportGUI.open(p);
-            }
-
-            case 19 -> {
-                p.closeInventory();
-                p.performCommand("townmenu");
-            }
-
-            case 21 -> {
-                p.closeInventory();
-                p.performCommand("jobs join");
-            }
-
+            case 10 -> { p.closeInventory(); BankGUI.open(p); }
+            case 12 -> { p.closeInventory(); ContractGUI.open(p); }
+            case 14 -> { p.closeInventory(); PriceGUI.open(p); }
+            case 16 -> { p.closeInventory(); TeleportGUI.open(p); }
+            case 19 -> { p.closeInventory(); p.performCommand("townmenu"); }
+            case 21 -> { p.closeInventory(); p.performCommand("jobs join"); }
             case 26 -> p.closeInventory();
         }
     }

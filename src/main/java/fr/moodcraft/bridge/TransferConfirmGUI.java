2de678650825c4884758d380f24dfb071fedad51
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 18:53:31 +0200

    Update TransferConfirmGUI.java

--- src/main/java/fr/moodcraft/bridge/TransferConfirmGUI.java
+++ src/main/java/fr/moodcraft/bridge/TransferConfirmGUI.java
@@ -9,65 +9,16 @@ public class TransferConfirmGUI {
 
     public static void open(Player p) {
 
-        TransferBuilder b = TransferBuilder.get(p);
-        if (b == null) return;
+        TransferBuilder data = TransferBuilder.get(p);
 
-        Inventory inv = Bukkit.createInventory(null, 27, "§eConfirmation virement");
+        Inventory inv = Bukkit.createInventory(null, 9, "§6Confirmer");
 
-        // ➖ -1000
-        SafeGUI.safeSet(inv, 10, SafeGUI.item(Material.RED_CONCRETE, "§4-1000",
-                "§8────────",
-                "§7Réduire fortement",
-                "",
-                "§7Montant actuel:",
-                "§f" + b.amount + "€"));
+        SafeGUI.safeSet(inv, 3,
+                SafeGUI.item(Material.REDSTONE, "§cAnnuler"));
 
-        // ➖ -100
-        SafeGUI.safeSet(inv, 11, SafeGUI.item(Material.REDSTONE, "§c-100",
-                "§8────────",
-                "§7Réduire le montant",
-                "",
-                "§7Montant actuel:",
-                "§f" + b.amount + "€"));
-
-        // 💰 CENTRE
-        SafeGUI.safeSet(inv, 13, SafeGUI.item(Material.GOLD_INGOT, "§eMontant",
-                "§8────────",
-                "§f" + b.amount + "€",
-                "",
-                "§7Cible:",
-                "§a" + (b.target == null ? "Non défini" : b.target.toString()),
-                "",
-                "§7Utilise les boutons"));
-
-        // ➕ +100
-        SafeGUI.safeSet(inv, 15, SafeGUI.item(Material.EMERALD, "§a+100",
-                "§8────────",
-                "§7Augmenter le montant",
-                "",
-                "§7Montant actuel:",
-                "§f" + b.amount + "€"));
-
-        // ➕ +1000
-        SafeGUI.safeSet(inv, 16, SafeGUI.item(Material.LIME_CONCRETE, "§2+1000",
-                "§8────────",
-                "§7Augmenter fortement",
-                "",
-                "§7Montant actuel:",
-                "§f" + b.amount + "€"));
-
-        // ❌ ANNULER
-        SafeGUI.safeSet(inv, 18, SafeGUI.item(Material.BARRIER, "§cAnnuler",
-                "§8────────",
-                "§7Annuler le virement"));
-
-        // ✅ VALIDER
-        SafeGUI.safeSet(inv, 26, SafeGUI.item(Material.LIME_DYE, "§aValider",
-                "§8────────",
-                "§7Confirmer le virement",
-                "",
-                "§7Montant:",
-                "§f" + b.amount + "€"));
+        SafeGUI.safeSet(inv, 5,
+                SafeGUI.item(Material.LIME_DYE, "§aConfirmer",
+                        "§7Montant: §f" + data.amount));
 
         GUIManager.open(p, "transfer_confirm", inv);
     }

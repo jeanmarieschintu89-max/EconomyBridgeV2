162971c9c9446eef009b917750f028e7b75ed814
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-03 23:46:15 +0200

    Create TopRepGUI.java

---
+++ src/main/java/fr/moodcraft/bridge/TopRepGUI.java
@@ -0,0 +1,53 @@
+package fr.moodcraft.bridge;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class TopRepGUI {
+
+    public static void open(org.bukkit.entity.Player p) {
+
+        Inventory inv = Bukkit.createInventory(null, 27, "§6Classement Réputation");
+
+        int slot = 10;
+        int i = 1;
+
+        for (Map.Entry<String, Integer> entry : ReputationManager.getTop(10).entrySet()) {
+
+            String uuid = entry.getKey();
+            int rep = entry.getValue();
+
+            String name = Bukkit.getOfflinePlayer(UUID.fromString(uuid)).getName();
+
+            ItemStack item = new ItemStack(Material.PLAYER_HEAD);
+            ItemMeta meta = item.getItemMeta();
+
+            if (meta != null) {
+
+                meta.setDisplayName("§e#" + i + " §f" + name);
+
+                meta.setLore(java.util.Arrays.asList(
+                        "§7Réputation: §a" + rep,
+                        "§7Statut: " + ReputationManager.getRank(rep),
+                        "",
+                        i == 1 ? "§6👑 Leader du serveur" : ""
+                ));
+
+                item.setItemMeta(meta);
+            }
+
+            inv.setItem(slot, item);
+
+            slot++;
+            i++;
+        }
+
+        p.openInventory(inv);
+    }
+}

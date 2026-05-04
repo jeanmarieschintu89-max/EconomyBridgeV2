353e40a8e607db2f8f5ae6aef3bf6c1228603c1a
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-04 21:26:06 +0200

    Update ContractManager.java

--- src/main/java/fr/moodcraft/bridge/ContractManager.java
+++ src/main/java/fr/moodcraft/bridge/ContractManager.java
@@ -19,8 +19,23 @@ public class ContractManager {
         return c;
     }
 
+    public static Contract get(int id) {
+        return ContractStorage.get(id);
+    }
+
+    public static List<Contract> getAll() {
+        return ContractStorage.getAll();
+    }
+
     public static List<Contract> getOpenContracts() {
         return ContractStorage.getOpen();
+    }
+
+    public static List<Contract> getByWorker(UUID uuid) {
+        return ContractStorage.getAll().stream()
+                .filter(c -> uuid.equals(c.acceptor)
+                        && c.status == Contract.Status.IN_PROGRESS)
+                .collect(Collectors.toList());
     }
 
     public static void remove(int id) {

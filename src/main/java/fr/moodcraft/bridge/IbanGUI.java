c7f4383b72f9483adda9e3813329925d9be0c9b6
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-04 15:58:25 +0200

    Create IbanGUI.java

---
+++ src/main/java/fr/moodcraft/bridge/IbanGUI.java
@@ -0,0 +1,25 @@
+package fr.moodcraft.bridge;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+public class IbanGUI {
+
+    public static void open(Player p) {
+
+        Inventory inv = Bukkit.createInventory(null, 9, "§bVirement IBAN");
+
+        SafeGUI.safeSet(inv, 4,
+                SafeGUI.item(Material.PAPER, "§eEntre l'IBAN dans le chat"));
+
+        SafeGUI.safeSet(inv, 8,
+                SafeGUI.item(Material.BARRIER, "§cRetour"));
+
+        GUIManager.open(p, "iban_gui", inv);
+
+        // 🔥 active input chat
+        InputManager.wait(p, "iban_input");
+    }
+}

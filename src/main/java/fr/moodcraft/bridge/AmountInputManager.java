706e4da5e43b9ed149e88c0ccf9ae278f0dd7e47
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-04 14:58:22 +0200

    Create AmountInputManager.java

---
+++ src/main/java/fr/moodcraft/bridge/AmountInputManager.java
@@ -0,0 +1,31 @@
+package fr.moodcraft.bridge;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class AmountInputManager {
+
+    public enum Type {
+        DEPOSIT,
+        WITHDRAW
+    }
+
+    private static final Map<UUID, Type> waiting = new HashMap<>();
+
+    public static void wait(Player p, Type type) {
+        waiting.put(p.getUniqueId(), type);
+    }
+
+    public static Type get(Player p) {
+        return waiting.get(p.getUniqueId());
+    }
+
+    public static void remove(Player p) {
+        waiting.remove(p.getUniqueId());
+    }
+
+    public static boolean isWaiting(Player p) {
+        return waiting.containsKey(p.getUniqueId());
+    }
+}

b219351e12a1f96943abfef95e67d9640c07864a
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-04 20:02:57 +0200

    Update TargetPlayerGUI.java

--- src/main/java/fr/moodcraft/bridge/TargetPlayerGUI.java
+++ src/main/java/fr/moodcraft/bridge/TargetPlayerGUI.java
@@ -5,17 +5,29 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 public class TargetPlayerGUI {
+
+    // 🔥 mapping slot → joueur
+    private static final Map<Integer, UUID> slotMap = new HashMap<>();
 
     public static void open(Player p) {
 
         Inventory inv = Bukkit.createInventory(null, 54, "§fChoix du joueur");
+
+        slotMap.clear(); // 🔥 IMPORTANT
 
         int slot = 0;
 
         for (Player target : Bukkit.getOnlinePlayers()) {
 
             if (target.equals(p)) continue;
+
+            // 🔥 on stocke le joueur
+            slotMap.put(slot, target.getUniqueId());
 
             SafeGUI.safeSet(inv, slot,
                     SafeGUI.item(Material.PLAYER_HEAD,
@@ -27,5 +39,9 @@ public class TargetPlayerGUI {
         }
 
         GUIManager.open(p, "bank_target", inv);
+    }
+
+    public static UUID getTarget(int slot) {
+        return slotMap.get(slot);
     }
 }

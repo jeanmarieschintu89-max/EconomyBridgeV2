d84644cba598dc27caabf62ac1186c98451c05d3
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-04 04:10:56 +0200

    Update InventoryCloseListener.java

--- src/main/java/fr/moodcraft/bridge/InventoryCloseListener.java
+++ src/main/java/fr/moodcraft/bridge/InventoryCloseListener.java
@@ -1,19 +1,16 @@
 package fr.moodcraft.bridge;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
+import org.bukkit.event.*;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 
 public class InventoryCloseListener implements Listener {
 
     @EventHandler
-    public void close(InventoryCloseEvent e) {
+    public void onClose(InventoryCloseEvent e) {
 
-        // 🔒 sécurité
         if (!(e.getPlayer() instanceof Player p)) return;
 
-        // 🧠 délègue au GUIManager (qui gère déjà le timing)
         GUIManager.close(p);
     }
 }

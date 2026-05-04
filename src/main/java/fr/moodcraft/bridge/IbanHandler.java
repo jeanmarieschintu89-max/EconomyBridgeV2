f68191f69fb24b424b367da7ad409fa5f6abf25f
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-04 18:58:32 +0200

    Update IbanHandler.java

--- src/main/java/fr/moodcraft/bridge/IbanHandler.java
+++ src/main/java/fr/moodcraft/bridge/IbanHandler.java
@@ -1,14 +1,31 @@
 package fr.moodcraft.bridge;
 
 import org.bukkit.entity.Player;
+import org.bukkit.metadata.FixedMetadataValue;
 
 public class IbanHandler implements GUIHandler {
 
     @Override
     public void onClick(Player p, int slot) {
 
-        if (slot == 8) {
-            TransferTypeGUI.open(p);
+        switch (slot) {
+
+            // 💳 saisir IBAN
+            case 4 -> {
+                p.closeInventory();
+
+                // 🔥 ACTIVE INPUT
+                p.setMetadata("input_active", new FixedMetadataValue(Main.getInstance(), true));
+
+                InputManager.wait(p, "iban_input");
+
+                p.sendMessage("§eEntre l'IBAN dans le chat.");
+            }
+
+            // 🔙 retour
+            case 8 -> {
+                BankGUI.open(p);
+            }
         }
     }
 }

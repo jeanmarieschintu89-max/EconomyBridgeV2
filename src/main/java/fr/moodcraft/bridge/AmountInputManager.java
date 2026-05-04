83909854620affc0e269ec3bae6784a57f591870
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-04 15:07:39 +0200

    Update AmountInputManager.java

--- src/main/java/fr/moodcraft/bridge/AmountInputManager.java
+++ src/main/java/fr/moodcraft/bridge/AmountInputManager.java
@@ -1,9 +1,10 @@
 package fr.moodcraft.bridge;
 
+import org.bukkit.entity.Player;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import org.bukkit.entity.Player;
 
 public class AmountInputManager {
 

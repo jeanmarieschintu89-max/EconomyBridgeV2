4f8e9c918fa29c5fa0112c334c68233aac39d84e
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-04 15:34:02 +0200

    Create InputManager.java

---
+++ src/main/java/fr/moodcraft/bridge/InputManager.java
@@ -0,0 +1,25 @@
+package fr.moodcraft.bridge;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class InputManager {
+
+    private static final HashMap<UUID, String> input = new HashMap<>();
+
+    public static void wait(Player p, String type) {
+        input.put(p.getUniqueId(), type);
+    }
+
+    public static String get(Player p) {
+        return input.get(p.getUniqueId());
+    }
+
+    public static void clear(Player p) {
+        input.remove(p.getUniqueId());
+    }
+
+    public static boolean has(Player p) {
+        return input.containsKey(p.getUniqueId());
+    }
+}

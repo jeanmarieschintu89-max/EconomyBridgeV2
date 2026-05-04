eedf9c054dad95c7e55db5dc1b263b0a37b33d85
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-04 17:55:43 +0200

    Update InputManager.java

--- src/main/java/fr/moodcraft/bridge/InputManager.java
+++ src/main/java/fr/moodcraft/bridge/InputManager.java
@@ -10,22 +10,18 @@ public class InputManager {
 
     private static final Map<UUID, String> inputs = new HashMap<>();
 
-    // 📥 définir un input attendu
-    public static void set(Player p, String type) {
+    public static void wait(Player p, String type) {
         inputs.put(p.getUniqueId(), type);
     }
 
-    // 🔍 vérifier si le joueur a un input
     public static boolean has(Player p) {
         return inputs.containsKey(p.getUniqueId());
     }
 
-    // 📤 récupérer le type
     public static String get(Player p) {
         return inputs.get(p.getUniqueId());
     }
 
-    // ❌ supprimer
     public static void clear(Player p) {
         inputs.remove(p.getUniqueId());
     }

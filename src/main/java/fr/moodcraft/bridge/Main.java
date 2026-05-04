985e4c2cba056218df748b97f0560bcf9bcd80d3
jeanmarieschintu89-max <jeanmarieschintu89@gmail.com>
2026-05-04 12:41:32 +0200

    Update Main.java

--- src/main/java/fr/moodcraft/bridge/Main.java
+++ src/main/java/fr/moodcraft/bridge/Main.java
@@ -80,7 +80,6 @@ public class Main extends JavaPlugin {
 
         // 🏆 CLASSEMENT (🔥 FIX ICI)
         GUIManager.register("top_rep", new TopRepHandler()); // ✔ TON GUI classement
-        GUIManager.register("profile_gui", new ProfileHandler());
 
         // 🧭 AUTRES
         GUIManager.register("teleport", new TeleportHandler());
